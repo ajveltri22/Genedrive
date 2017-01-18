4f0f14170844b018543b0465ecfc6a22e93287c5
Anthony Veltri <ajveltri22@gmail.com>
2017-01-18 00:34:12 -0500

    Updated mate chance to pass unpenalized mate_chance to offspring and distinguish between inherited mate_chance and penalized mate_chance. Added get_mate_chance_average method to Population.

--- genedrive.py
+++ genedrive.py
@@ -7,25 +7,27 @@ from string import ascii_uppercase as generation_ID
 
 #TODO: make sure Population.check_parameters() is exhaustive
 #TODO: data output formatting
+# TODO: double check mating chance logic
+# assumes conversion only happens in germ line cells "Meiotic drive"
 
 class Parameters():
     def __init__(self):
         self.generations = 1000           #number of generations to run
-        self.initial_pop_size = 100            #total population size
+        self.initial_pop_size = 1000  #total population size
         self.population_cap = 7000         #Must be 'False' or a number greater than initial_pop_size
-        self.num_introduced = 5          #number of organisms introduced
-        self.inherit_chance = .9          #chance of inheritance of gene drive from heterozygous individual
+        self.num_introduced = 1  # number of organisms introduced
+        self.inherit_chance = .3  #chance of inheritance of gene drive from heterozygous individual
         self.inherit_from_mother = True
         self.inherit_from_father = True
-        self.mate_chance_average = 1   #chance to mate in generation
-        self.mate_chance_stdev = 0
-        self.male_reproduction_penalty = 0.1
-        self.female_reproduction_penalty = 0.1
+        self.initial_mate_chance_average = .9  # chance to mate in generation
+        self.mate_chance_stdev = .05
+        self.male_reproduction_penalty = 0
+        self.female_reproduction_penalty = 0
         self.max_litter_size = 1             #number of offspring per mating event
         self.max_lifespan = 5  # number of generations an individual lives
         self.stochastic_death = True
-        self.avg_age_of_death = 5
-        self.stdev_age_of_death = 1
+        self.avg_age_of_death = 4
+        self.stdev_age_of_death = .5
         self.parameter_check()
 
     def parameter_check(self):
@@ -47,8 +49,8 @@ class Parameters():
             raise Exception("Inheritance must be turned on for at least one parent if num_introduced is nonzero.")
         if self.inherit_chance < 0 or self.inherit_chance > 1:
             raise Exception("inherit_chance must be between 0 and 1 inclusive.")
-        if self.mate_chance_average < 0:
-            raise Exception("mate_chance_average must be zero or greater.")
+        if self.initial_mate_chance_average < 0:
+            raise Exception("initial_mate_chance_average must be zero or greater.")
         if self.mate_chance_stdev < 0:
             raise Exception("mate_chance_stdev must be zero or greater.")
         if self.male_reproduction_penalty < 0 or self.female_reproduction_penalty < 0:
@@ -60,7 +62,7 @@ class Parameters():
         if type(self.stochastic_death) != bool:
             raise Exception("stochastic_death must be a boolean.")
         if self.avg_age_of_death < 0:
-            raise Exception("mate_chance_average must be zero or greater.")
+            raise Exception("initial_mate_chance_average must be zero or greater.")
         if self.stdev_age_of_death < 0:
             raise Exception("mate_chance_stdev must be zero or greater.")
 
@@ -86,24 +88,25 @@ class Population():
     def create_generation_A(self):
         for i in range(self.parameters.initial_pop_size):
             identifier = str(self.current_generation)+"_"+str(i+1)
+            assigned_mate_chance = gauss(self.parameters.initial_mate_chance_average, self.parameters.mate_chance_stdev)
             if self.parameters.num_introduced > 0:
                 if self.parameters.inherit_from_mother and self.parameters.inherit_from_father:
                     self.members[identifier] = \
-                        self.Individual(self.parameters, self.parameters.inherit_chance, gauss(self.parameters.mate_chance_average, self.parameters.mate_chance_stdev))
+                        self.Individual(self.parameters, self.parameters.inherit_chance, assigned_mate_chance)
                     self.parameters.num_introduced -= 1
                 elif self.parameters.inherit_from_mother and not self.parameters.inherit_from_father:
                     self.members[identifier] = \
                         self.Individual(self.parameters, self.parameters.inherit_chance,
-                                   gauss(self.parameters.mate_chance_average, self.parameters.mate_chance_stdev), "F")
+                                        assigned_mate_chance, "F")
                     self.parameters.num_introduced -= 1
                 elif self.parameters.inherit_from_father and not self.parameters.inherit_from_mother:
                     self.members[identifier] = \
                         self.Individual(self.parameters, self.parameters.inherit_chance,
-                                   gauss(self.parameters.mate_chance_average, self.parameters.mate_chance_stdev), "M")
+                                        assigned_mate_chance, "M")
                     self.parameters.num_introduced -= 1
             else:
                 self.members[identifier] = \
-                    self.Individual(self.parameters, 0, gauss(self.parameters.mate_chance_average, self.parameters.mate_chance_stdev))
+                    self.Individual(self.parameters, 0, assigned_mate_chance)
 
             if self.members[identifier].sex == "M":
                 self.male_list.append(identifier)
@@ -166,45 +169,42 @@ class Population():
                 if random() <= self.members[i[0]].mate_chance*self.members[i[1]].mate_chance and population_uncapped: #if True, pair mates
                     self.cumulative_number_of_individuals += 1
                     offspring_identifier = str(self.current_generation)+"_"+str(self.cumulative_number_of_individuals)
-                    avg_mate_chance = (self.members[i[0]].mate_chance + self.members[i[1]].mate_chance) / 2
+                    avg_mate_chance = (self.members[i[0]].assigned_mate_chance + self.members[
+                        i[1]].assigned_mate_chance) / 2
                     mother_pass_chance = self.members[i[0]].pass_gene_chance
                     father_pass_chance = self.members[i[1]].pass_gene_chance
                     frandom = random()
                     mrandom = random()
-
+                    assigned_mate_chance = 0
+                    while assigned_mate_chance == 0 or assigned_mate_chance > 1:
+                        assigned_mate_chance = gauss(avg_mate_chance, self.parameters.mate_chance_stdev)
                     if self.parameters.inherit_from_father and self.parameters.inherit_from_mother:
                         if mrandom > mother_pass_chance and frandom > father_pass_chance:
                             self.members[offspring_identifier] = self.Individual(self.parameters, 0,
-                                                                                  gauss(avg_mate_chance,
-                                                                                        self.parameters.mate_chance_stdev))
+                                                                                 assigned_mate_chance)
                         elif mrandom <= mother_pass_chance and frandom <= father_pass_chance:
                             self.members[offspring_identifier] = self.Individual(self.parameters, 1,
-                                                                                  gauss(avg_mate_chance,
-                                                                                        self.parameters.mate_chance_stdev))
+                                                                                 assigned_mate_chance)
                         elif mrandom <= mother_pass_chance or frandom <= father_pass_chance:
-                            self.members[offspring_identifier] = self.Individual(self.parameters, self.parameters.inherit_chance,
-                                                                                  gauss(avg_mate_chance,
-                                                                                        self.parameters.mate_chance_stdev))
+                            self.members[offspring_identifier] = self.Individual(self.parameters,
+                                                                                 self.parameters.inherit_chance,
+                                                                                 assigned_mate_chance)
                     elif self.parameters.inherit_from_father and not self.parameters.inherit_from_mother:
                         if frandom > father_pass_chance:
                             self.members[offspring_identifier] = self.Individual(self.parameters, 0,
-                                                                                  gauss(avg_mate_chance,
-                                                                                        self.parameters.mate_chance_stdev))
+                                                                                 assigned_mate_chance)
                         elif frandom <= father_pass_chance:
                             self.members[offspring_identifier] = self.Individual(self.parameters,
-                                                                                  self.parameters.inherit_chance,
-                                                                                  gauss(avg_mate_chance,
-                                                                                        self.parameters.mate_chance_stdev))
+                                                                                 self.parameters.inherit_chance,
+                                                                                 assigned_mate_chance)
                     elif self.parameters.inherit_from_mother and not self.parameters.inherit_from_father:
                         if mrandom > mother_pass_chance:
                             self.members[offspring_identifier] = self.Individual(self.parameters, 0,
-                                                                                  gauss(avg_mate_chance,
-                                                                                        self.parameters.mate_chance_stdev))
+                                                                                 assigned_mate_chance)
                         elif mrandom <= mother_pass_chance:
                             self.members[offspring_identifier] = self.Individual(self.parameters,
-                                                                                  self.parameters.inherit_chance,
-                                                                                  gauss(avg_mate_chance,
-                                                                                        self.parameters.mate_chance_stdev))
+                                                                                 self.parameters.inherit_chance,
+                                                                                 assigned_mate_chance)
                     self.ancestry[offspring_identifier] = i
                     if self.members[offspring_identifier].sex == "M":
                         self.male_list.append(offspring_identifier)
@@ -219,6 +219,16 @@ class Population():
                         self.homozygous_carrier_list.append(offspring_identifier)
             else:
                 pass
+
+    def get_mate_chance_average(self):
+        mate_chance_sum = 0
+        for i in self.members:
+            mate_chance_sum += self.members[i].mate_chance
+        if len(self.members) == 0:
+            return 0
+        else:
+            population_average_mate_chance = mate_chance_sum / len(self.members)
+            return population_average_mate_chance
 
     def get_carrier_percentage(self):
         if len(self.members) == 0:
@@ -238,29 +248,36 @@ class Population():
             else: self.sex = sex
             self.age = 0
             self.pass_gene_chance = pass_gene_chance
+            self.assigned_mate_chance = mate_chance
+            if self.assigned_mate_chance > 1:
+                self.assigned_mate_chance = 1
             if self.pass_gene_chance > 0:
                 if self.sex=="M":
-                    self.mate_chance = mate_chance - self.parameters.male_reproduction_penalty
+                    self.mate_chance = self.assigned_mate_chance - self.parameters.male_reproduction_penalty
                 if self.sex=="F":
-                    self.mate_chance = mate_chance - self.parameters.female_reproduction_penalty
+                    self.mate_chance = self.assigned_mate_chance - self.parameters.female_reproduction_penalty
             else:
                 self.mate_chance = mate_chance
-
 
 
 def main():
     parameters = Parameters()
     population = Population(parameters)
-    print(len(population.members))
-
+    x = population.get_carrier_percentage()
+    num_introduced = parameters.num_introduced
+    print(population.current_generation, len(population.members), x, population.get_mate_chance_average())
     for i in range(parameters.generations):
         population.mate()
-        print(len(population.members), population.get_carrier_percentage())
+        x = population.get_carrier_percentage()
+        print(population.current_generation, len(population.members), x, population.get_mate_chance_average())
         if len(population.members)==0:
-            print("Population gone at generation "+str(population.current_generation)+", "+str(population.cumulative_number_of_individuals)+" total individuals.")
+            print("Population zero at generation " + str(population.current_generation) + ", " + str(
+                population.cumulative_number_of_individuals) + " total individuals.")
             break
-
-
+        elif x == 0 and num_introduced != 0:
+            print("Gene drive gone at generation " + str(population.current_generation) + ", " + str(
+                population.cumulative_number_of_individuals) + " total individuals.")
+            break
 
 
 main()
